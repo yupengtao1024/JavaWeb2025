62a52da92807ca469d92a261fd303ff1c4aedff9
yupengtao <yupengtao1024@gmail.com>
2025-03-05 15:56:28 +0800

    HelloServlet

---
+++ HelloServlet/src/main/java/com/this0/servlet/FruitServlet.java
@@ -0,0 +1,23 @@
+package com.this0.servlet;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class FruitServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        String fname = req.getParameter("fname");
+        Integer price = Integer.valueOf(req.getParameter("price"));
+        Integer fcount = Integer.valueOf(req.getParameter("fcount"));
+        String remark = req.getParameter("remark");
+
+        System.out.println("fname=" + fname);
+        System.out.println("price=" + price);
+        System.out.println("fcount=" + fcount);
+        System.out.println("remark=" + remark);
+    }
+}
