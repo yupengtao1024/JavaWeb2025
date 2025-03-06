b9a2a5072e9fbbae9c14cd5b8136f40f67c746be
yupengtao <yupengtao1024@gmail.com>
2025-03-06 15:18:21 +0800

    注解方式配置servlet

--- HelloServlet/src/main/java/com/this0/servlet/FruitServlet.java
+++ HelloServlet/src/main/java/com/this0/servlet/FruitServlet.java
@@ -1,11 +1,13 @@
 package com.this0.servlet;
 
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+@WebServlet("/fruit")
 public class FruitServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
