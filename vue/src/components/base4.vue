5d1f10297f36b03e03c75e45fddba37dbbce058b
yupengtao <yupengtao1024@gmail.com>
2025-03-28 03:12:11 +0800

    ref实现响应式

--- vue/src/components/base4.vue
+++ vue/src/components/base4.vue
@@ -1,8 +1,10 @@
 <script type="module" setup>
-let counter = 0;
+import {ref} from "vue";
+
+let counter = ref(0);
 
 function show() {
-  alert(counter);
+  alert(counter.value);
 }
 </script>
 
@@ -12,7 +14,6 @@ function show() {
     {{ counter }}
     <button @click="counter++">+</button>
     <hr>
-    <!-- 此案例,我们发现counter值,会改变,但是页面不改变! 默认Vue3的数据是非响应式的!-->
     <button @click="show()">显示counter值</button>
   </div>
 </template>
