0d073061b21a23210c346648b303a7bad3cbba10
yupengtao <yupengtao1024@gmail.com>
2025-07-14 12:50:44 +0800

    使用计算属性之后

--- vue/src/components/base8.vue
+++ vue/src/components/base8.vue
@@ -6,16 +6,31 @@ const author = reactive({
   books: [
     'Vue 2 - Advanced Guide',
     'Vue 3 - Basic Guide',
-    'Vue 4 - The Mystery',
-  ],
+    'Vue 4 - The Mystery'
+  ]
 })
+
+// 一个计算属性
+const publishedBooksMessage = computed(() => {
+  console.log("publishedBooksMessage")
+  return author.books.length > 0 ? 'Yes' : 'No'
+})
+// 一个函数
+let hasBooks = () => {
+  console.log("hasBooks")
+  return author.books.length > 0 ? 'Yes' : 'no'
+}
 </script>
 
 <template>
   <div>
     <p>{{ author.name }} Has published books?:</p>
     <span>{{ author.books.length > 0 ? 'Yes' : 'No' }}</span>
+    <span>{{ hasBooks() }}</span><!-- 调用方法,每个标签都会调用一次 -->
+    <span>{{ hasBooks() }}</span>
   </div>
+  <span>{{ publishedBooksMessage }}</span><!-- 属性计算,属性值不变时,多个个标签只会调用一次 -->
+  <span>{{ publishedBooksMessage }}</span>
 </template>
 
 <style scoped></style>
