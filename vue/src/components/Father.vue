58cd808ea08e537ced69013f9174e10f2e9d285b
yupengtao <yupengtao1024@gmail.com>
2025-07-15 23:52:53 +0800

    组件传值：子传父

--- vue/src/components/Father.vue
+++ vue/src/components/Father.vue
@@ -5,12 +5,20 @@ import {ref} from "vue";
 
 //1.父传子
 const money = ref(100);
+
+function moneyMinis(arg){
+
+  // alert("感知到儿子买了棒棒糖"+arg);
+  money.value +=arg;
+}
 </script>
 
 <template>
   <div style="background-color: #747bff">
     <h2>Father</h2>
-    <Son :money="money"></Son>
+
+<!--    购买事件-->
+    <Son :money="money" @buy="moneyMinis"/>
   </div>
 </template>
 
--- vue/src/components/Son.vue
+++ vue/src/components/Son.vue
@@ -1,11 +1,21 @@
 <script setup>
 let props = defineProps(['money']);
+
+//1.defineEmits用于定义要发送给父组件的方法，可以1个或者多个
+let emits = defineEmits(['buy']);
+
+function buy(){
+  emits('buy',-5);
+}
 </script>
 
 <template>
   <div style="background-color: #888888;color: antiquewhite">
     <h3>Son</h3>
     <div>账户：{{ props.money }}</div>
+
+<!--    2.提交数据的方法-->
+    <button @click="buy">买棒棒糖</button>
   </div>
 </template>
 
