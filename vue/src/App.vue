0ea1914c0d4d0e6d748d38fb88f2458ec4db0c73
yupengtao <yupengtao1024@gmail.com>
2025-07-30 19:21:36 +0800

    vue 通过async和await处理异步请求

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -4,25 +4,19 @@ import {onMounted, reactive} from 'vue';
 
 let jsonData = reactive({code: 1, content: '我努力不是为了你而是因为你'})
 
-let getLoveMessage = () => {
-  axios({
+let getLoveWords = () => {
+  return axios({
     method: "post", // 请求方式
     url: "http://api.uomg.com/api/rand.qinghua?format=json",  // 请求的url
-    data: { // 当请求方式为post时,data下的数据以JSON串放入请求体,否则以key=value形式放url后
+    data: {// 当请求方式为post时,data下的数据以JSON串放入请求体,否则以key=value形式放url后
       username: "123456"
     }
-  }).then(
-      (response) => {
-        console.log(response)//响应成功时要执行的函数
-        Object.assign(jsonData, response.data)  //复制响应数据
-      }
-  ).catch(
-      (error) => {
+  })
+}
 
-        // 响应失败时要执行的函数
-        console.log(error)
-      }
-  )
+let getLoveMessage = async () => {
+  let {data} = await getLoveWords()
+  Object.assign(jsonData, data)
 }
 
 /* 通过onMounted生命周期,自动加载一次 */
