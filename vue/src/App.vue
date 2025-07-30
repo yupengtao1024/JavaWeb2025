5fe157b09ac33e5ff1eee6b4660f825138341263
yupengtao <yupengtao1024@gmail.com>
2025-07-31 00:44:05 +0800

    vue axios拦截器测试

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,5 +1,6 @@
 <script setup type="module">
-import axios from 'axios'
+// 导入我们自己定义的axios.js文件,而不是导入axios依赖
+import axios from './axios.js'
 import {onMounted, ref, reactive, toRaw} from 'vue';
 
 let jsonData = reactive({code: 1, content: '我努力不是为了你而是因为你'})
@@ -7,20 +8,16 @@ let jsonData = reactive({code: 1, content: '我努力不是为了你而是因为
 let getLoveWords = () => {
   try {
     return axios.post(
-        'http://api.uomg.com/api/rand.qinghua',
-        {//请求体中的JSON数据
+        'api/rand.qinghua',
+        {
           username: 'zhangsan',
           password: '123456'
-        },
-        {// 其他参数
-          params: {// url上拼接的键值对参数
+        },//请求体中的JSON数据
+        {
+          params: {
             format: 'json',
-          },
-          headers: {// 请求头
-            'Accept': 'application/json, text/plain, text/html,*/*',
-            'X-Requested-With': 'XMLHttpRequest'
           }
-        }
+        }// 其他键值对参数
     )
   } catch (e) {
     return e
@@ -28,10 +25,16 @@ let getLoveWords = () => {
 }
 
 let getLoveMessage = async () => {
-  let {data} = await getLoveWords()
-  Object.assign(jsonData, data)
-}
 
+  // 这里返回的是一个fulfilled（已兑现）状态的promise
+  getLoveWords().then(
+      (response) => {
+        console.log("after getloveWords")
+        console.log(response)
+        Object.assign(jsonData, response.data)
+      }
+  )
+}
 /* 通过onMounted生命周期,自动加载一次 */
 onMounted(() => {
   getLoveMessage()
